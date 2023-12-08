2d085880d64df4d079a3ccaaebe44e8d1f8d18e6
RohanSharma4792 <rohan.sharma2020@vitbhopal.ac.in>
2023-12-08 22:02:56 +0530

    auth done

---
+++ src/appwrite.config.js
@@ -0,0 +1,8 @@
+import { Client } from 'appwrite';
+
+const client = new Client();
+
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject('656235965a4cbff68eeb');
+
+export default client;
+
