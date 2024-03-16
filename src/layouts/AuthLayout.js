2ab73f4856640b96b62b8676206408247daa8e72
RohanSharma4792 <rohan.sharma2020@vitbhopal.ac.in>
2024-03-16 19:49:17 +0530

    auth correct

--- src/layouts/AuthLayout.js
+++ src/layouts/AuthLayout.js
@@ -39,14 +39,7 @@ function AuthLayout() {
                 <span className="mx-80">Or</span>
                 <span>
                 <div class="flex items-center justify-center">
-                    <button
-                    //  onClick={(e)=>googleAuth(e)}
-                    class="mx-30 px-20 py-2 border flex gap-2 border-black-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-black-200 hover:slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition duration-150">
-                        <img class="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/>
-                        <span>Login with Google</span>
-                    {/* button for google */}
-                      
-                    </button> 
+                    
                   </div>
                     </span>
                   New to Attendease? Create an account{" "}
